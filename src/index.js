7942286149d2a5acf91718c85890f6ca1367517b
Mika Reich <mikareich@Judiths-MacBook-Pro.local>
2020-09-30 19:39:57 +0200

    add trigger function for navbar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.scss'
 import './responsive.scss'
+
 // drawer
+const drawerTriggerDIV = document.querySelector('#drawerTrigger')
+const navBar = document.querySelector('.nav')
+drawerTriggerDIV.addEventListener('click', () =>
+  navBar.classList.toggle('collapsed')
+)
