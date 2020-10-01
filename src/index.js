79a167f7a5eefd141e3fa58bf61b41665996bac7
Mika Reich <mikareich@Judiths-MacBook-Pro.local>
2020-10-01 14:58:12 +0200

    create parallax effect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import './style.scss'
-
-// drawer
-const drawerTriggerDIV = document.querySelector('#drawerTrigger')
-const navBar = document.querySelector('.nav')
-drawerTriggerDIV.addEventListener('click', () =>
-  navBar.classList.toggle('collapsed')
-)
