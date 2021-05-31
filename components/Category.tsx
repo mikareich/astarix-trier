b6052ab566fb3d0da1a0f0f52566f33b6f3a91a2
Mika Reich <mikareich0@gmail.com>
2021-05-31 16:45:14 +0200

    add id

--- components/Category.tsx
+++ components/Category.tsx
@@ -4,9 +4,9 @@ import { ICategory } from "../interfaces";
 import categoryStyles from "../styles/Category.module.scss";
 import MenuItem from "./MenuItem";
 
-function Category({ title, products, note }: ICategory) {
+function Category({ title, products, note, id }: ICategory) {
   return (
-    <section className={categoryStyles.category}>
+    <section className={categoryStyles.category} id={id}>
       <h3>{title}</h3>
       <p>{note}</p>
       <div className={categoryStyles.products}>{products.map(MenuItem)}</div>
