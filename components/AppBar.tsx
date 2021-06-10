7575a75d2398f6c1c324a17802c39ab2570ccc9c
Mika Reich <mikareich0@gmail.com>
2021-06-10 19:18:47 +0200

    update appbar

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -32,7 +32,8 @@ const AppBar: React.FC<AppProps> = ({ routes, position }) => {
                     }`}
       >
         {routes.map(({ title, slug, leading }) => {
-          const isActive = router.asPath === `/${slug}`;
+          const isActive = (router.query.slug || router.route) === slug;
+          console.log(router);
           const className = `
             ${appBarStyles.link} 
             ${leading ? appBarStyles.leading : "inactive"}
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import React, { useEffect, useRef, useState } from "react";
 
 import { Category as CategoryProps } from "../interfaces";
